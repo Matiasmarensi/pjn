cbf231cee974a584159b6f0eb1916cf417e405c5
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-10-10 21:27:45 -0300

    CAPTCHA

--- index.js
+++ index.js
@@ -43,9 +43,40 @@ async function openBrowser() {
     await page2.select(selectSelector, "10");
     console.log("Opción seleccionada en el dropdown: COM");
     console.log(selectSelector);
+    const inputNum = await page2.waitForSelector("#formPublica\\:numero");
+    console.log("Input de número encontrado.");
+    await inputNum.type("1203");
+    const inputAni = await page2.waitForSelector("#formPublica\\:anio");
+    await inputAni.type("2023");
+    //
+    // si hay captcha
 
-    // Cerrar el navegador (opcional)
-    // await browser.close();
+    const captchaSelector = "#formPublica\\:recaptcha-publico\\:reCaptcha"; // Selector del CAPTCHA
+    const captchaExists = await page2.$(captchaSelector); // Verifica si el elemento existe
+
+    if (captchaExists) {
+      console.log("Captcha encontrado. Por favor resuélvelo manualmente.");
+      // Espera 60 segundos para que el usuario resuelva el CAPTCHA
+      await new Promise((resolve) => setTimeout(resolve, 60000));
+    } else {
+      console.log("Captcha no encontrado.");
+    }
+
+    // Esperar a que el usuario complete el CAPTCHA
+    console.log("Presiona Enter cuando hayas completado el CAPTCHA...");
+    await page2.evaluate(() => {
+      return new Promise((resolve) => {
+        const confirmation = confirm("¿Has completado el CAPTCHA?");
+        if (confirmation) {
+          resolve();
+        }
+      });
+    });
+    // Hacer clic en el botón de consultar
+    const consultarButtonSelector = "#formPublica\\:buscarPorNumeroButton"; // Selector del botón
+    const consultarButton = await page2.waitForSelector(consultarButtonSelector);
+    await consultarButton.click();
+    console.log("Botón de consulta clickeado.");
   } catch (error) {
     console.log("Error:", error);
   }
