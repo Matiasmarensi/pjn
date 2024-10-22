90fd8cf2d17d13b24bb7cd9bafc62d12444848c8
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-10-22 18:43:04 -0300

    input out

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Input from "../components/Input";
 
 const Login = () => {
   const [email, setEmail] = useState("");
